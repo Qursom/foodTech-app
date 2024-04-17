ea50a424488384a341ba49d3c49ef96723dd81a0
Qursom <57573505+Qursom@users.noreply.github.com>
2024-04-17 19:39:40 +0500

    Update database.config.ts

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,14 +1,28 @@
-import { connect,ConnectOptions } from "mongoose";
+// import { connect,ConnectOptions } from "mongoose";
 
-export const  dbConnect=()=>{
+// export const  dbConnect=()=>{
   
-    connect(process.env.MONGO_URI!,{
-         useNewUrlParser: true,
-         useUnifiedTopology: true
-    } as ConnectOptions).then(
-        ()=>console.log("DB connection successful"),
-        (error) => console.log("Db connection failed",error)
-    )
+//     connect(process.env.MONGO_URI!,{
+//         // useNewUrlParser: true,
+//         // useUnifiedTopology: true
+//     } as ConnectOptions).then(
+//         ()=>console.log("DB connection successful"),
+//         (error) => console.log("Db connection failed",error)
+//     )
+// }
+
+
+import mongoose from "mongoose";
+
+
+const dbConnect = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.Mongo_URI}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
 }
 
 export default dbConnect
